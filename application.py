5d1e52f2d8f5f36b24bc4abe8e9346dd01a184b7
woonzh <zhenhao1991@gmail.com>
2018-08-13 17:08:20 +0800

    t

--- application.py
+++ application.py
@@ -4,11 +4,10 @@ Created on Sat Mar 24 17:34:54 2018
 
 @author: woon.zhenhao
 """
-import flask
+
 from flask import Flask, request, make_response, render_template, redirect
 from flask_cors import CORS
 from flask_restful import Resource, Api
-import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,4 +23,5 @@ def main():
 
 
 if __name__ == '__main__':
-     app.run(debug=True)
+     app.debug=True
+     app.run()
