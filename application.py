a0cfa6f7d070acba12a8ba29e4add0fbaf1feea7
woonzh <zhenhao1991@gmail.com>
2018-08-13 17:29:45 +0800

    t

--- application.py
+++ application.py
@@ -5,13 +5,13 @@ Created on Sat Mar 24 17:34:54 2018
 @author: woon.zhenhao
 """
 
-from flask import Flask, request, make_response, render_template, redirect
-from flask_cors import CORS
-from flask_restful import Resource, Api
-
-app = Flask(__name__)
-api = Api(app)
-CORS(app)
+#from flask import Flask, request, make_response, render_template, redirect
+#from flask_cors import CORS
+#from flask_restful import Resource, Api
+#
+#app = Flask(__name__)
+#api = Api(app)
+#CORS(app)
 
 #@app.route('/')
 #def hello():
@@ -21,6 +21,13 @@ CORS(app)
 #def main():
 #    return render_template('index.html')
 
+#if __name__ == '__main__':
+#     app.debug=True
+#     app.run(host='0.0.0.0')
+     
+from flask import Flask
+
+# print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
@@ -35,17 +42,20 @@ home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
 # add a rule for the index page.
-app.add_url_rule('/', 'index', (lambda: header_text +
+application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-app.add_url_rule('/<username>', 'hello', (lambda username:
+application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
-
-if __name__ == '__main__':
-     app.debug=True
-     app.run(host='0.0.0.0')
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
